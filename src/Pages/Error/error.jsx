83d1b77424c84639222790b28770f5e93a5480fa
gilsonmak <gilsonmakanounou@yahoo.com>
2024-10-25 14:26:52 +0200

    feat: affichage de la page 404

---
+++ src/Pages/Error/error.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./error.scss"
+import { Link } from 'react-router-dom'
+
+const Error = () => {
+  return (
+    <main className='error-main'>
+      <p className="error-404">404</p>
+      <p className="error-message">Oups! La page que vous cherchez n'existe pas.</p>
+      <Link className='link' to="/">Retourner sur la page d'accueil</Link>
+    </main>
+  )
+}
+
+export default Error
